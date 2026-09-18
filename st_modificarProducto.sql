ae8ffcf14074536341404bbdf82f79488e6b1801
Willian-vargas01 <willian.vargas.et32@gmail.com>
2026-09-18 19:56:53 -0300

    sp_modificarProducto

--- st_modificarProducto.sql
+++ st_modificarProducto.sql
@@ -1,5 +1,5 @@
 delimiter //
-	create procedure st_modificarProducto(in p_id_inventario int, in p_nombre_producto varchar(100), in p_tipo varchar(20), in p_unidad varchar(20), in p_stock_minimo decimal(10,2))
+	create procedure sp_modificarProducto(in p_id_inventario int, in p_nombre_producto varchar(100), in p_tipo varchar(20), in p_unidad varchar(20), in p_stock_minimo decimal(10,2))
 begin
 	update inventario
     set nombre_producto = p_nombre_producto,
